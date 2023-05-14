e08a726289035ad8c11c292553a572d97637ecf0
sajjad <sreyhani@ce.sharif.edu>
2023-05-14 15:18:09 +0330

    make it work on stm32l051c8tx

--- stm32f3_blinky_v1/src/main.rs
+++ stm32f3_blinky_v1/src/main.rs
@@ -6,20 +6,24 @@
 use panic_rtt_target as _;
 use rtic::app;
 use rtt_target::{rprintln, rtt_init_print};
-use stm32f3xx_hal::gpio::{Output, PushPull, PA5};
-use stm32f3xx_hal::prelude::*;
-use systick_monotonic::{fugit::Duration, Systick};
 
-#[app(device = stm32f3xx_hal::pac, peripherals = true, dispatchers = [SPI1])]
+#[app(device = stm32l0xx_hal::pac, peripherals = true, dispatchers = [SPI1])]
 mod app {
     use super::*;
-
+    use systick_monotonic::{fugit::Duration, Systick};
+    use stm32l0xx_hal::gpio::gpiob::PB0;
+    use stm32l0xx_hal::gpio::{Output, PushPull};
+    use stm32l0xx_hal::rcc::RccExt;
+    use stm32l0xx_hal::rcc::Config;
+    use stm32l0xx_hal::gpio::GpioExt;
+    use stm32l0xx_hal::prelude::OutputPin;
+    
     #[shared]
     struct Shared {}
 
     #[local]
     struct Local {
-        led: PA5<Output<PushPull>>,
+        led: PB0<Output<PushPull>>,
         state: bool,
     }
 
@@ -29,26 +33,19 @@ mod app {
     #[init]
     fn init(cx: init::Context) -> (Shared, Local, init::Monotonics) {
         // Setup clocks
-        let mut flash = cx.device.FLASH.constrain();
-        let mut rcc = cx.device.RCC.constrain();
+        let mut rcc = cx.device.RCC.freeze(Config::hsi16());
 
         let mono = Systick::new(cx.core.SYST, 36_000_000);
 
         rtt_init_print!();
         rprintln!("init");
 
-        let _clocks = rcc
-            .cfgr
-            .use_hse(8.MHz())
-            .sysclk(36.MHz())
-            .pclk1(36.MHz())
-            .freeze(&mut flash.acr);
 
         // Setup LED
-        let mut gpioa = cx.device.GPIOA.split(&mut rcc.ahb);
-        let mut led = gpioa
-            .pa5
-            .into_push_pull_output(&mut gpioa.moder, &mut gpioa.otyper);
+        let gpiob = cx.device.GPIOB.split(&mut rcc);
+        let mut led = gpiob
+            .pb0
+            .into_push_pull_output();
         led.set_high().unwrap();
 
         // Schedule the blinking task
@@ -71,6 +68,6 @@ mod app {
             cx.local.led.set_low().unwrap();
             *cx.local.state = true;
         }
-        blink::spawn_after(Duration::<u64, 1, 1000>::from_ticks(1000)).unwrap();
+        blink::spawn_after(Duration::<u64, 1, 1000>::from_ticks(200)).unwrap();
     }
 }
